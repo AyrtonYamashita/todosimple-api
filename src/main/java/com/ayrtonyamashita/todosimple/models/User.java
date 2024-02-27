2b50e6bb1a3e0d2bd6281faa3e925e06b792c275
Ayrton Yamashita <ayrtonyamashita@gmail.com>
2024-02-27 08:28:55 -0400

    Create Task model

--- src/main/java/com/ayrtonyamashita/todosimple/models/User.java
+++ src/main/java/com/ayrtonyamashita/todosimple/models/User.java
@@ -1,5 +1,7 @@
 package com.ayrtonyamashita.todosimple.models;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -10,6 +12,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -46,7 +49,9 @@ public class User {
     @Size(groups = { CreateUser.class, UpdateUser.class }, min = 8, max = 60)
     private String password;
 
-    // private List<Task> tasks = new ArrayList<Task>();
+
+    @OneToMany(mappedBy = "user")
+    private List<Task> tasks = new ArrayList<Task>();
 
     public User() {
     }
