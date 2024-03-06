52b96ddd24a768b76e5e198352a059d68b7e6798
Ayrton Yamashita <ayrtonyamashita@gmail.com>
2024-03-06 10:33:17 -0400

    Create User Spring Security

--- src/main/java/com/ayrtonyamashita/todosimple/models/User.java
+++ src/main/java/com/ayrtonyamashita/todosimple/models/User.java
@@ -30,7 +30,6 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-import org.springframework.context.annotation.Profile;
 
 @Entity
 @Table(name = User.TABLE_NAME)
